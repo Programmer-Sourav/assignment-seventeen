e5d282eb9e929b0086c3bd88bbd35af77f4be945
Sourav-nath <35804676+Programmer-Sourav@users.noreply.github.com>
2023-09-26 19:16:14 +0530

    forms
    forms

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import ExcerciseForm from './pages/ExcerciseForm';
+import Example from './pages/Example';
+import GoalTracking from './pages/GoalTracking';
+import FoodTracking from './pages/FoodTracking';
 
 function App() {
   return (
     <div className="App">
        <ExcerciseForm/>
+       <GoalTracking/>
+       {/* <FoodTracking/> */}
+       
     </div>
   );
 }
--- src/pages/ExcerciseForm.jsx
+++ src/pages/ExcerciseForm.jsx
@@ -1,45 +1,35 @@
 import "../stylesheets/excerciseform.css"
 
-import React from 'react';
+import React, { useReducer } from 'react';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
-import Joi from 'joi';
-import { initialState } from "../reducer/formReducer";
+import formReducer, { initialState } from "../reducer/formReducer";
 
 export default function ExcerciseForm (){
 
 
-    const validationSchema = Joi.object({
-        excerciseName: Joi.string().required,
-        duration: Joi.number().required, 
-        caloriesBurned: Joi.number().required
-    })
-
-    const handleSubmit = (values) =>{
-        console.log("Form Data ", values)
-    }
-
-    const handleFieldChange = (e) =>{
-
-    }
+    const [state, dispatch ] = useReducer(formReducer, initialState)
     return(
         <div className='container'>
             <h1 className="header">Excercise Tracker</h1>
             <div className='form-container'>
-        <Formik initialValues={initialState} validationSchema={validationSchema} onSubmit={handleSubmit}>
+        <Formik initialValues={state.excerciseDetails} onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        console.log(JSON.stringify(values, null, 2));
+      }}>
             <Form> 
              <div className='form-group'> 
              <label htmlFor='excercise' className="textleft">Name of the Excercise</label>
-             <Field type='text' id="excercise" name='excercise' placeholder='Excercise' onChange={handleFieldChange}/>
+             <Field type='text' id="excercise" name="excerciseName" placeholder='Excercise' />
              <ErrorMessage name="excercise" component="div" className='error'/>
              </div> 
              <div className='form-group' >
              <label htmlFor='duration' className="textleft">Duration of the Excercise</label>  
-             <Field type='number' id='duration' name='duration' placeholder='10 mins' onChange={handleFieldChange}/>
+             <Field type='number' id='duration' name='duration' placeholder='10 mins' />
              <ErrorMessage name="duration" component="div" className='error'/>
              </div>
              <div className='form-group'>
              <label htmlFor='caloriesBurned' className="textleft">Calories Burned</label>  
-             <Field type='number' id='caloriesBurned' name='caloriesBurned' placeholder='100 KCAL' onChange={handleFieldChange}/>
+             <Field type='number' id='caloriesBurned' name='caloriesBurned' placeholder='100 KCAL' />
              <ErrorMessage name="caloriesBurned" component="div" className='error'/>
              </div>
              <button type="submit" className="basicbutton">Submit</button>
